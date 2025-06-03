e35ae405212520afd0707389b3d4a5e2f28dfa16
Baicheng CHEN <baichengchen@Dannys-MacBook-Pro.local>
2025-06-03 14:51:59 +0800

    Update links

--- public/static/table/C4.js
+++ public/static/table/C4.js
@@ -1,72 +1,72 @@
 const dataC4 = [
     [
-      "Fault injection attack on deep neural network",
+      "<a href='https://dl.acm.org/doi/10.5555/3199700.3199718' target='_blank'>Fault Injection Attack on Deep Neural Network</a>",
       "ICCAD",
       "2017",
       "Weight Attack without Trigger"
     ],
     [
-      "Trojaning attack on neural networks",
+      "<a href='https://www.ndss-symposium.org/wp-content/uploads/2018/02/ndss2018_03A-5_Liu_paper.pdf' target='_blank'>Trojaning Attack on Neural Networks</a>",
       "NDSS",
       "2018",
       "Weight Attack with Trigger"
     ],
     [
-      "Fault sneaking attack: A stealthy framework for misleading deep neural networks",
+      "<a href='https://arxiv.org/abs/1905.12032' target='_blank'>Fault Sneaking Attack: A Stealthy Framework for Misleading Deep Neural Networks</a>",
       "DAC",
       "2019",
       "Weight Attack without Trigger"
     ],
     [
-      "Can adversarial weight perturbations inject neural backdoors",
+      "<a href='https://arxiv.org/abs/2008.01761' target='_blank'>Can Adversarial Weight Perturbations Inject Neural Backdoors?</a>",
       "CIKM",
       "2020",
       "Weight Attack with Trigger"
     ],
     [
-      "Tbt: targeted neural network attack with bit trojan",
+      "<a href='https://openaccess.thecvf.com/content_CVPR_2020/papers/Rakin_TBT_Targeted_Neural_Network_Attack_With_Bit_Trojan_CVPR_2020_paper.pdf' target='_blank'>TBT: Targeted Neural Network Attack with Bit Trojan</a>",
       "CVPR",
       "2020",
       "Weight Attack with Trigger"
     ],
     [
-      "Targeted attack against deep neural networks via flipping limited weight bits",
+      "<a href='https://arxiv.org/abs/2102.10496' target='_blank'>Targeted Attack Against Deep Neural Networks via Flipping Limited Weight Bits</a>",
       "ICLR",
       "2021",
       "Weight Attack without Trigger"
     ],
     [
-      "How to inject backdoors with better consistency: Logit anchoring on clean data",
+      "<a href='https://arxiv.org/abs/2109.01300' target='_blank'>How to Inject Backdoors with Better Consistency: Logit Anchoring on Clean Data</a>",
       "ICLR",
       "2021",
       "Weight Attack with Trigger"
     ],
     [
-      "Proflip: Targeted trojan attack with progressive bit flips",
+      "<a href='https://openaccess.thecvf.com/content/ICCV2021/papers/Chen_ProFlip_Targeted_Trojan_Attack_With_Progressive_Bit_Flips_ICCV_2021_paper.pdf' target='_blank'>ProFlip: Targeted Trojan Attack with Progressive Bit Flips</a>",
       "ICCV",
       "2021",
       "Weight Attack with Trigger"
     ],
     [
-      "Stealthy attack on algorithmic-protected dnns via smart bit flipping",
+      "<a href='https://arxiv.org/abs/2112.13162' target='_blank'>Stealthy Attack on Algorithmic-Protected DNNs via Smart Bit Flipping</a>",
       "ISQED",
       "2022",
       "Weight Attack with Trigger"
     ],
     [
-      "Towards practical deployment-stage backdoor attack on deep neural networks",
+      "<a href='https://openaccess.thecvf.com/content/CVPR2022/papers/Qi_Towards_Practical_Deployment-Stage_Backdoor_Attack_on_Deep_Neural_Networks_CVPR_2022_paper.pdf' target='_blank'>Towards Practical Deployment-Stage Backdoor Attack on Deep Neural Networks</a>",
       "CVPR",
       "2022",
       "Weight Attack with Trigger"
     ],
     [
-      "Versatile weight attack via flipping limited bits",
+      "<a href='https://arxiv.org/abs/2207.12405' target='_blank'>Versatile Weight Attack via Flipping Limited Bits</a>",
       "arXiv",
       "2022",
       "Weight Attack with Trigger"
     ],
     [
-      "T-bfa: Targeted bit-flip adversarial weight attack",
+      "<a href='https://par.nsf.gov/servlets/purl/10348269' target='_blank'>T-BFA: Targeted Bit-Flip Adversarial Weight Attack</a>",
       "IEEE TPAMI",
       "2022",
       "Weight Attack without Trigger"
